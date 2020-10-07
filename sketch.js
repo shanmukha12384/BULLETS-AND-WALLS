922ae47906005b2e71f99b7223fc060c0fffdbca
shanmukha12384 <71809117+shanmukha12384@users.noreply.github.com>
2020-10-07 20:04:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var bullet,wall,thickness;
 var speed,weight;
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(1326,400);
   speed=random(55,90)
   weight=random(400,1500)
   bullet=createSprite(50,200,50,50);
